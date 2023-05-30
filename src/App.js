b14080a4c596e4541195fecce2efcdedce4ef254
Webby-lab <keviczkyne.kover.krisztina@gmail.com>
2023-05-30 19:42:53 +0200

    react app created with base map

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import MapComponent from "./MapBasemap";
+
+function App() {
+  return (
+    <div className="App">
+      <MapComponent />
+      
+    </div>
+  );
+}
+
+export default App;
